820743b773faf746bb6e6729a18c141dd3345082
jenil shingala <jenilshingala28@gmail.com>
2025-09-25 11:05:49 +0530

    rechange

--- src/App.jsx
+++ src/App.jsx
@@ -62,7 +62,12 @@ import { OfferPerResto } from "./components/restaurants/OfferPerResto";
 import { UpdateAdPro } from "./components/admin/UpdateAdPro";
 
 function App() {
-  axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+  // axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+  axios.defaults.baseURL = "http://localhost:3000";
+  // Make sure this uses the environment variable
+  // axios.defaults.baseURL =
+  //   import.meta.env.VITE_API_URL || "http://localhost:3000";
+  axios.defaults.withCredentials = true; // ne
   const location = useLocation();
 
   useEffect(() => {
